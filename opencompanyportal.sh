8822c13213019ba5fb26c577da5452a1d9478675
andybowser <andybowser@users.noreply.github.com>
2025-08-26 13:34:16 +1000

    Update opencompanyportal.sh

--- opencompanyportal.sh
+++ opencompanyportal.sh
@@ -1,2 +1,3 @@
-CurrentUser=$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ { print $3 }')
-sudo -u $CurrentUser /usr/bin/open "/Applications/Company Portal.app"
+#!/bin/zsh
+
+/usr/bin/open "/Applications/Company Portal.app"
